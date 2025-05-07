8f76159037fb5b063f9634ad01c47cbeffbc8582
hanjungho <thegamerhan@gmail.com>
2025-05-07 18:43:32 +0900

    feat: mcp

--- src/main/java/org/lucky0111/pettalk/domain/dto/review/ReviewUpdateDTO.java
+++
@@ -1,9 +0,0 @@
-package org.lucky0111.pettalk.domain.dto.review;
-
-public record ReviewUpdateDTO(
-        Integer rating,
-        String title,
-        String comment,
-        String reviewImageUrl
-) {
-}
--- src/main/java/org/lucky0111/pettalk/repository/community/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package org.lucky0111.pettalk.repository.community;
-
-import org.lucky0111.pettalk.domain.entity.community.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
